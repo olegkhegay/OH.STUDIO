d8f034ab1b6d9cf0bd2b4ede4b26fa46d7a7705f
Oleg <oxegay@gmail.com>
2024-10-28 17:09:52 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss'
+import React from 'react'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Catolog from './components/Catolog/Catolog'
+
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Catolog/>
+    </>
+  )
+}
+
+export default App
